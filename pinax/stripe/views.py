d66f692aadea14612f7e356f7f919b599c0e6f18
Oliver Zhou <zhou.oliver@gmail.com>
2017-01-25 17:12:25 -0800

    Remove old views

--- pinax/stripe/views.py
+++ pinax/stripe/views.py
@@ -60,15 +60,6 @@ class StripeView(APIView):
             return Customer.create(self.request.user)
 
 
-# class InvoiceListView(LoginRequiredMixin, CustomerMixin, ListView):
-#     model = Invoice
-#     context_object_name = "invoice_list"
-#     template_name = "pinax/stripe/invoice_list.html"
-
-#     def get_queryset(self):
-#         return super(InvoiceListView, self).get_queryset().order_by("date")
-
-
 class InvoiceListView(StripeView, generics.ListAPIView, CustomerMixin):
     '''DRF APIView List Invoices'''
     queryset = Invoice.objects.all()
@@ -138,15 +129,6 @@ class PaymentMethodView(StripeView, generics.RetrieveUpdateDestroyAPIView, Custo
             return self.form_invalid(form)
 
 
-# class PaymentMethodListView(LoginRequiredMixin, CustomerMixin, ListView):
-#     model = Card
-#     context_object_name = "payment_method_list"
-#     template_name = "pinax/stripe/paymentmethod_list.html"
-
-#     def get_queryset(self):
-#         return super(PaymentMethodListView, self).get_queryset().order_by("created_at")
-
-
 class PaymentMethodListView(StripeView, generics.ListAPIView, CustomerMixin):
     '''DRF APIView List Invoices'''
     queryset = Card.objects.all()
@@ -155,21 +137,6 @@ class PaymentMethodListView(StripeView, generics.ListAPIView, CustomerMixin):
     def get_queryset(self):
         customer = self.get_customer()
         return customer.cards.all()
-
-
-# class PaymentMethodCreateView(LoginRequiredMixin, CustomerMixin, PaymentsContextMixin, TemplateView):
-#     model = Card
-#     template_name = "pinax/stripe/paymentmethod_create.html"
-
-#     def create_card(self, stripe_token):
-#         sources.create_card(self.customer, token=stripe_token)
-
-#     def post(self, request, *args, **kwargs):
-#         try:
-#             self.create_card(request.POST.get("stripeToken"))
-#             return redirect("pinax_stripe_payment_method_list")
-#         except stripe.CardError as e:
-#             return self.render_to_response(self.get_context_data(errors=smart_str(e)))
 
 
 class PaymentMethodCreateView(StripeView, generics.CreateAPIView, CustomerMixin, PaymentsContextMixin):
@@ -191,22 +158,6 @@ class PaymentMethodCreateView(StripeView, generics.CreateAPIView, CustomerMixin,
             return Response(self.get_context_data(errors=smart_str(e)))
 
 
-# class PaymentMethodDeleteView(LoginRequiredMixin, CustomerMixin, DetailView):
-#     model = Card
-#     template_name = "pinax/stripe/paymentmethod_delete.html"
-
-#     def delete_card(self, stripe_id):
-#         sources.delete_card(self.customer, stripe_id)
-
-#     def post(self, request, *args, **kwargs):
-#         self.object = self.get_object()
-#         try:
-#             self.delete_card(self.object.stripe_id)
-#             return redirect("pinax_stripe_payment_method_list")
-#         except stripe.CardError as e:
-#             return self.render_to_response(self.get_context_data(errors=smart_str(e)))
-
-
 class PaymentMethodDeleteView(StripeView, generics.DestroyAPIView, CustomerMixin):
     '''DRF APIView Delete Payment Method (Card)'''
     queryset = Card.objects.all()
@@ -223,30 +174,6 @@ class PaymentMethodDeleteView(StripeView, generics.DestroyAPIView, CustomerMixin
             return redirect("pinax_stripe_payment_method_list")
         except stripe.CardError as e:
             return Response(self.get_context_data(errors=smart_str(e)))
-
-
-# class PaymentMethodUpdateView(LoginRequiredMixin, CustomerMixin, PaymentsContextMixin, FormMixin, DetailView):
-#     model = Card
-#     form_class = PaymentMethodForm
-#     template_name = "pinax/stripe/paymentmethod_update.html"
-
-#     def update_card(self, exp_month, exp_year):
-#         sources.update_card(self.customer, self.object.stripe_id, exp_month=exp_month, exp_year=exp_year)
-
-#     def form_valid(self, form):
-#         try:
-#             self.update_card(form.cleaned_data["expMonth"], form.cleaned_data["expYear"])
-#             return redirect("pinax_stripe_payment_method_list")
-#         except stripe.CardError as e:
-#             return self.render_to_response(self.get_context_data(errors=smart_str(e)))
-
-#     def post(self, request, *args, **kwargs):
-#         self.object = self.get_object()
-#         form = self.get_form(form_class=self.form_class)
-#         if form.is_valid():
-#             return self.form_valid(form)
-#         else:
-#             return self.form_invalid(form)
 
 
 class PaymentMethodUpdateView(StripeView, generics.UpdateAPIView, CustomerMixin, PaymentsContextMixin):
@@ -277,30 +204,6 @@ class SubscriptionView(StripeView):
     serializer_class = SubscriptionSerializer
     queryset = Subscription.objects.all()
 
-    # def get(self, request, *args, **kwargs):
-    #     current_subscription = self.get_current_subscription()
-    #     serializer = SubscriptionSerializer(current_subscription)
-    #     return Response(serializer.data, status=status.HTTP_200_OK)
-
-    # def post(self, request, *args, **kwargs):
-    #     try:
-    #         serializer = self.serializer_class(data=request.data)
-
-    #         if serializer.is_valid():
-    #             validated_data = serializer.validated_data
-    #             stripe_plan = validated_data.get('stripe_plan', None)
-    #             customer = self.get_customer()
-    #             subscription = customer.subscribe(stripe_plan)
-
-    #             return Response(subscription, status=status.HTTP_201_CREATED)
-    #         else:
-    #             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    #     except stripe.StripeError as e:
-    #         from django.utils.encoding import smart_str
-
-    #         error_data = {u'error': smart_str(e) or u'Unknown error'}
-    #         return Response(error_data, status=status.HTTP_400_BAD_REQUEST)
-
 
 class SubscriptionViewSet(StripeView, viewsets.ModelViewSet, CustomerMixin):
     ''' Combine Subscription ListView, CreateView, DeleteView, UpdateView '''
@@ -308,41 +211,9 @@ class SubscriptionViewSet(StripeView, viewsets.ModelViewSet, CustomerMixin):
     serializer_class = SubscriptionSerializer
 
 
-# class SubscriptionListView(LoginRequiredMixin, CustomerMixin, ListView):
-#     model = Subscription
-#     context_object_name = "subscription_list"
-#     template_name = "pinax/stripe/subscription_list.html"
-
-#     def get_queryset(self):
-#         return super(SubscriptionListView, self).get_queryset().order_by("created_at")
-
 class SubscriptionListView(StripeView, generics.ListAPIView, CustomerMixin):
     queryset = Subscription.objects.all()
     serializer_class = SubscriptionSerializer
-
-
-# class SubscriptionCreateView(LoginRequiredMixin, PaymentsContextMixin, CustomerMixin, FormView):
-#     template_name = "pinax/stripe/subscription_create.html"
-#     form_class = PlanForm
-
-#     @property
-#     def tax_percent(self):
-#         return settings.PINAX_STRIPE_SUBSCRIPTION_TAX_PERCENT
-
-#     def set_customer(self):
-#         if self.customer is None:
-#             self._customer = customers.create(self.request.user)
-
-#     def subscribe(self, customer, plan, token):
-#         subscriptions.create(customer, plan, token=token, tax_percent=self.tax_percent)
-
-#     def form_valid(self, form):
-#         self.set_customer()
-#         try:
-#             self.subscribe(self.customer, plan=form.cleaned_data["plan"], token=self.request.POST.get("stripeToken"))
-#             return redirect("pinax_stripe_subscription_list")
-#         except stripe.StripeError as e:
-#             return self.render_to_response(self.get_context_data(form=form, errors=smart_str(e)))
 
 
 class SubscriptionCreateView(StripeView, generics.CreateAPIView, PaymentsContextMixin, CustomerMixin):
@@ -369,22 +240,6 @@ class SubscriptionCreateView(StripeView, generics.CreateAPIView, PaymentsContext
         #     return Response(self.get_context_data(errors=smart_str(e)))
 
 
-# class SubscriptionDeleteView(LoginRequiredMixin, CustomerMixin, DetailView):
-#     model = Subscription
-#     template_name = "pinax/stripe/subscription_delete.html"
-
-#     def cancel(self):
-#         subscriptions.cancel(self.object)
-
-#     def post(self, request, *args, **kwargs):
-#         self.object = self.get_object()
-#         try:
-#             self.cancel()
-#             return redirect("pinax_stripe_subscription_list")
-#         except stripe.StripeError as e:
-#             return self.render_to_response(self.get_context_data(errors=smart_str(e)))
-
-
 class SubscriptionDeleteView(StripeView, generics.DestroyAPIView, PaymentsContextMixin, CustomerMixin):
     '''Sets specific Subscription to end.
 
@@ -404,49 +259,6 @@ class SubscriptionDeleteView(StripeView, generics.DestroyAPIView, PaymentsContex
             return Response(self.get_context_data(errors=smart_str(e)))
 
 
-# class SubscriptionUpdateView(LoginRequiredMixin, CustomerMixin, FormMixin, DetailView):
-#     model = Subscription
-#     form_class = PlanForm
-#     template_name = "pinax/stripe/subscription_update.html"
-
-#     @property
-#     def current_plan(self):
-#         if not hasattr(self, "_current_plan"):
-#             self._current_plan = self.object.plan
-#         return self._current_plan
-
-#     def get_context_data(self, **kwargs):
-#         context = super(SubscriptionUpdateView, self).get_context_data(**kwargs)
-#         context.update({
-#             "form": self.get_form(form_class=self.form_class)
-#         })
-#         return context
-
-#     def update_subscription(self, plan_id):
-#         subscriptions.update(self.object, plan_id)
-
-#     def get_initial(self):
-#         initial = super(SubscriptionUpdateView, self).get_initial()
-#         initial.update({
-#             "plan": self.current_plan
-#         })
-#         return initial
-
-#     def form_valid(self, form):
-#         try:
-#             self.update_subscription(form.cleaned_data["plan"])
-#             return redirect("pinax_stripe_subscription_list")
-#         except stripe.StripeError as e:
-#             return self.render_to_response(self.get_context_data(form=form, errors=smart_str(e)))
-
-#     def post(self, request, *args, **kwargs):
-#         self.object = self.get_object()
-#         form = self.get_form(form_class=self.form_class)
-#         if form.is_valid():
-#             return self.form_valid(form)
-#         else:
-#             return self.form_invalid(form)
-
 class SubscriptionUpdateView(StripeView, generics.UpdateAPIView, PaymentsContextMixin, CustomerMixin):
     '''Not validated fully yet'''
     queryset = Subscription.objects.all()
@@ -457,13 +269,6 @@ class SubscriptionUpdateView(StripeView, generics.UpdateAPIView, PaymentsContext
         if not hasattr(self, "_current_plan"):
             self._current_plan = self.object.plan
         return self._current_plan
-
-    # def get_context_data(self, **kwargs):
-    #     context = super(SubscriptionUpdateView, self).get_context_data(**kwargs)
-    #     context.update({
-    #         "form": self.get_form(form_class=self.form_class)
-    #     })
-    #     return context
 
     def update_subscription(self, plan_id):
         subscriptions.update(self.object, plan_id)
@@ -504,31 +309,6 @@ class SubscriptionUpdateView(StripeView, generics.UpdateAPIView, PaymentsContext
                             self.serializer_class.errors,
                             status=status.HTTP_400_BAD_REQUEST,
                             )
-
-
-# class Webhook(View):
-
-#     @method_decorator(csrf_exempt)
-#     def dispatch(self, *args, **kwargs):
-#         return super(Webhook, self).dispatch(*args, **kwargs)
-
-#     def extract_json(self):
-#         data = json.loads(smart_str(self.request.body))
-#         return data
-
-#     def post(self, request, *args, **kwargs):
-#         data = self.extract_json()
-#         if events.dupe_event_exists(data["id"]):
-#             exceptions.log_exception(data, "Duplicate event record")
-#         else:
-#             events.add_event(
-#                 stripe_id=data["id"],
-#                 kind=data["type"],
-#                 livemode=data["livemode"],
-#                 message=data
-#             )
-#         return HttpResponse()
-
 
 
 class Webhook(StripeView):
